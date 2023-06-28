11f3b17bf1652ed54f0cf06b46e6737825700c5d
Cisco-A <ifeanyifavour74@gmail.com>
2023-06-28 16:09:35 -0700

    Testing basic puppet commands

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+file { '/tmp/school':
+    mode    =>    '0744',
+    owner   =>   'www-data',
+    path    =>    '/tmp/school',
+    group   =>   'www-data',
+    content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+Tasks on Configuration Management Using Puppet
